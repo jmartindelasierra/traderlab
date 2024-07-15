c275cb6acc112a6146aaf473cfc380d602084643
jmartindelasierra <jesus.martinsierra@gmail.com>
2024-07-15 22:31:03 +0200

    fix average bars computation

--- R/avg_bars_per_trade.R
+++ R/avg_bars_per_trade.R
@@ -5,7 +5,10 @@
 #'
 avg_bars_per_trade <- function(ohlcv_data) {
 
-  (rle(ohlcv_data$trade)$lengths[rle(ohlcv_data$trade)$values == 1] - 1) |>
+  ohlcv_data |>
+    dplyr::filter(!is.na(bars_from_entry),
+                  bars_from_entry != 0) |>
+    dplyr::pull(bars_from_entry) |>
     mean()
 
 }
