16644a6b0fd995a340ce52509b6bb0c63afe1b04
jmartindelasierra <jesus.martinsierra@gmail.com>
2025-01-24 16:05:29 +0100

    Add metrics

---
+++ R/avg_balance_percent_winner.R
@@ -0,0 +1,17 @@
+
+#' Average percent winner from balance
+#'
+#' @param ohlcv_data A data.frame with OHLCV data.
+#'
+avg_balance_percent_winner <- function(ohlcv_data) {
+
+  # Initialization to avoid notes in R CMD check
+  balance <- NULL
+
+  ohlcv_data |>
+    dplyr::mutate(pct_return = (balance - dplyr::lag(balance)) / dplyr::lag(balance)) |>
+    dplyr::filter(pct_return > 0) |>
+    dplyr::pull(pct_return) |>
+    mean(na.rm = TRUE)
+
+}
