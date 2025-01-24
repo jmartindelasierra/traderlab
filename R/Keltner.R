6a4c48e4f5a285394e63dbf99a7438ef92b4e42d
jmartindelasierra <jesus.martinsierra@gmail.com>
2025-01-24 15:52:59 +0100

    Fix ATR multiplier parameter name

--- R/Keltner.R
+++ R/Keltner.R
@@ -2,14 +2,14 @@
 #' Keltner channels
 #'
 #' @param ohlcv_data A data.frame with OHLCV data.
-#' @param periods An integer with number of periods.
-#' @param atr_periods An integer with number of ATR periods.
+#' @param periods An integer for number of periods.
+#' @param atr_mult A number for ATR multiplier.
 #'
-#' @return This function returns 'lwr', 'avg', 'upr' and 'pct' series.
+#' @return This function returns 'lwr', 'avg' and 'upr' series.
 #'
-Keltner <- function(ohlcv_data, periods, atr_periods) {
+Keltner <- function(ohlcv_data, periods, atr_mult) {
 
-  channels <- TTR::keltnerChannels(ohlcv_data[, c("high", "low", "close")], n = periods, atr = atr_periods)
+  channels <- TTR::keltnerChannels(ohlcv_data[, c("high", "low", "close")], n = periods, atr = atr_mult)
 
   as.data.frame(channels) |>
     stats::setNames(c("lwr", "avg", "upr"))
